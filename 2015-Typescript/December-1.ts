bfa3432cc3021c01f76ce67433a50dae1f6fa13d
Caleb O'Neal <caleboneal07@gmail.com>
2025-10-13 18:56:32 -0400

    Solve december 1 part 2

--- 2015-Typescript/December-1.ts
+++ 2015-Typescript/December-1.ts
@@ -3,6 +3,7 @@ const input = Bun.file("input1");
 const text = await input.text();
 
 var output = 0;
+var count = 0;
 
 for (const char of text) {
   if (char == "(") {
@@ -10,6 +11,11 @@ for (const char of text) {
   } else if (char == ")") {
     output -= 1;
   }
+  count += 1;
+  if (output == -1) {
+    break;
+  }
 }
 
 console.log(output);
+console.log(count);
