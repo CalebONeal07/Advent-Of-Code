c7c6dac50c8e574a8dd6c303478b6f1ba831d237
Caleb O'Neal <caleboneal07@gmail.com>
2025-10-13 19:20:31 -0400

    Solve part 1 of 2015 december 2

---
+++ 2015-Typescript/December-2.ts
@@ -0,0 +1,32 @@
+const input = Bun.file("input/december2");
+const text: string[] = (await input.text()).split("\r\n");
+
+var sum = 0;
+
+for (const i in text) {
+  const line = text[i];
+  if (line != null && line != "") {
+    const dimensions: number[] = line.split("x").map((x) => parseInt(x));
+
+    if (
+      dimensions[0] == null ||
+      dimensions[1] == null ||
+      dimensions[2] == null
+    ) {
+      console.log("Line skipped: " + line);
+      continue;
+    }
+
+    const l: number = dimensions[0];
+    const w: number = dimensions[1];
+    const h: number = dimensions[2];
+
+    sum += 2 * l * w + 2 * w * h + 2 * h * l;
+    const min = [l * w, w * h, h * l].sort((a, b) => a - b)[0];
+    if (min != null) {
+      sum += min;
+    }
+  }
+}
+
+console.log(sum);
